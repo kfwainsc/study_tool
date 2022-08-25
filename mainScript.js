9132c2f8034b2c24bdd55301f7f432e5883079ce
Kendra <63425365+kfwainsc@users.noreply.github.com>
2022-08-25 14:04:52 -0700

    Delete mainScript.js

--- mainScript.js
+++
@@ -1,7 +0,0 @@
-const emergencyBtn = document.getElementById("emergency");
-emergencyBtn.addEventListener(
-  "click",
-  (callAlert = () => {
-    alert("GET OFF THIS PAGE and call 9-1-1");
-  })
-);
